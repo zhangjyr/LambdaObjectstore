6b5a855ee7cf61d0c39a902c252b27c4cda40420
Ao Wang <awang24@gmu.edu>
2019-05-22 16:06:27 -0400

    clean

--- Lambda1/Lambda1.go
+++ Lambda1/Lambda1.go
@@ -1,218 +1,26 @@
-package main
+package Lambda1
 
 import (
 	"fmt"
-	"github.com/google/uuid"
-	"github.com/kelindar/binary"
-	"io"
-	"net"
-	"time"
+	"github.com/gomodule/redigo/redis"
 )
 
-type header struct {
-	Name      string
-	TimeStamp time.Time
-	Uuid      [16]byte
-}
-
-type body struct {
-	obj []byte
-}
-
-type Frame struct {
-	lenHeader uint32
-	lenBody   uint32
-	header    []byte
-	body      []byte
-}
-
-var frameHandler = make(chan *Frame, 100)
-
-func encodeHeader(funcName string) []byte {
-	myHeader := &header{
-		Name:      funcName,
-		TimeStamp: time.Now(),
-		Uuid:      uuid.New(),
-	}
-	encoded, err := binary.Marshal(myHeader)
-	if err != nil {
-		fmt.Println("encode err", err)
-	}
-	return encoded
-}
-
-func encodeBody() []byte {
-	myBody := &body{
-		obj: []byte{1, 2, 3},
-	}
-	encoded, err := binary.Marshal(myBody)
-	if err != nil {
-		fmt.Println("encode err", err)
-	}
-	return encoded
-}
-
-func connect() *net.TCPConn {
-	tcpAddr, err := net.ResolveTCPAddr("tcp", "52.201.234.235:8080") // ec2 address
-	//tcpAddr, err := net.ResolveTCPAddr("tcp", "localhost:3333") // ec2 address
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	tcpConn, err := net.DialTCP("tcp", nil, tcpAddr) //dial tcp
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-	return tcpConn
-}
-
-func tcpSend(conn *net.TCPConn, headerLen []byte, bodyLen []byte, header []byte, body []byte) {
-	//fmt.Println(headerLen)
-	//fmt.Println(bodyLen)
-	//fmt.Println(header)
-	//fmt.Println(body)
-
-	sendCount := 0
-	switch sendCount {
-	case 0:
-		_, err := conn.Write(headerLen)
-		if err != nil {
-			fmt.Println(err)
-		}
-		sendCount = 1
-		fallthrough
-	case 1:
-		_, err := conn.Write(bodyLen)
-		if err != nil {
-			fmt.Println(err)
-		}
-		sendCount = 2
-		fallthrough
-	case 2:
-		_, err := conn.Write(header)
-		if err != nil {
-			fmt.Println(err)
-		}
-		sendCount = 3
-		fallthrough
-	case 4:
-		_, err := conn.Write(body)
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
-
+type DialConfig struct {
+	Network string
+	Address string
 }
 
 func main() {
-	tcpCoon := connect()
-	defer tcpCoon.Close() //close conn
-
-	myHeader := encodeHeader("Lambda2SmallJPG")
-	myBody := encodeBody()
-
-	myHeaderLen := make([]byte, 4)
-	binary.BigEndian.PutUint32(myHeaderLen, uint32(len(myHeader))) // get header length
-
-	myBodyLen := make([]byte, 4)
-	binary.BigEndian.PutUint32(myBodyLen, uint32(len(myBody))) // get body length
-
-	tcpSend(tcpCoon, myHeaderLen, myBodyLen, myHeader, myBody)
-
-	// receive from server
-	go receive(tcpCoon)
-	fmt.Println(<-frameHandler)
-}
-func receive(conn *net.TCPConn) {
-	time.Sleep(1 * time.Second)
-	//defer conn.Close()
-	frame := &Frame{
-		lenHeader: 0,
-		lenBody:   0,
-		header:    []byte{},
-		body:      []byte{},
+	//c, err := redis.Dial("tcp", ":3333")
+	c, err := redis.Dial("tcp", "52.201.234.235:8080")
+	if err != nil {
+		// handle error
+		fmt.Println(err)
 	}
-	intBuff := make([]byte, 4)
-	fieldFrame := 0
+	n, _ := redis.Bytes(c.Do("GET", "k1"))
+	fmt.Println(n)
+	//n, _ = redis.Int(c.Do("INCR", "k1"))
+	//fmt.Printf("%#v\n", n)
 
-	buff := make([]byte, 2048)
-	for {
-		buffFrom := 0
-		n, err := conn.Read(buff)
-		fmt.Println("n is", n)
-		if err == io.EOF {
-			fmt.Println(conn.RemoteAddr(), "has disconnect")
-			fmt.Println(err)
-			break
-		} else if err != nil {
-			fmt.Println(conn.RemoteAddr(), "connection error")
-			fmt.Println(err)
-			break
-		}
-
-		switch fieldFrame {
-		case 0:
-			// get header length
-			if n-buffFrom >= cap(intBuff) {
-				frame.lenHeader = binary.BigEndian.Uint32(buff[buffFrom : buffFrom+4])
-				buffFrom = buffFrom + 4
-				fieldFrame = 1
-				fmt.Println("rec case0")
-				goto nextCase0
-			} else {
-				copy(intBuff, buff[buffFrom:n])
-			}
-		nextCase0:
-			fallthrough
-		case 1:
-			// get body length
-			if n-buffFrom >= cap(intBuff) {
-				frame.lenBody = binary.BigEndian.Uint32(buff[buffFrom : buffFrom+4])
-				buffFrom = buffFrom + 4
-				fieldFrame = 2
-				fmt.Println("rec case1")
-				goto nextCase1
-
-			} else {
-				copy(intBuff, buff[buffFrom:n])
-			}
-		nextCase1:
-			fallthrough
-		case 2:
-			// get header
-			if n-buffFrom >= int(frame.lenHeader)-len(frame.header) {
-				frame.header = merge(frame.header, buff[buffFrom:buffFrom+int(frame.lenHeader)])
-				buffFrom = buffFrom + len(frame.header)
-				fmt.Println("len header", len(frame.header))
-				fieldFrame = 3
-				fmt.Println("rec case2")
-				goto nextCase2
-			} else {
-				frame.header = merge(frame.header, buff[buffFrom:])
-			}
-		nextCase2:
-			fallthrough
-		case 3:
-			// get body
-			if n-buffFrom >= int(frame.lenBody)-len(frame.body) {
-				frame.body = merge(frame.body, buff[buffFrom:n])
-				buffFrom = buffFrom + len(frame.body)
-				fmt.Println("len header", len(frame.body))
-				fmt.Println("rec case3")
-				fieldFrame = 0
-			} else {
-				frame.body = merge(frame.body, buff[:n])
-			}
-
-		}
-		frameHandler <- frame
-	}
-}
-func merge(a []byte, b []byte) []byte {
-	c := make([]byte, len(a)+len(b))
-	copy(c, a)
-	copy(c[len(a):], b)
-	return c
+	defer c.Close()
 }
---
+++ Lambda2/lambda2.go
@@ -0,0 +1,30 @@
+package Lambda2
+
+import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/gomodule/redigo/redis"
+)
+
+var c, ok = redis.Dial("tcp", "52.201.234.235:8080")
+
+func HandleRequest() {
+	fmt.Println("client", c)
+	if ok != nil {
+		// handle error
+		fmt.Println("client err", ok)
+	}
+	n, err := c.Do("SET", "k1", []byte{233, 1})
+	if err != nil {
+		fmt.Println("Set error", err)
+	}
+	fmt.Println("set complete", n)
+	//n, _ = redis.Int(c.Do("INCR", "k1"))
+	//fmt.Printf("%#v\n", n)
+
+	//defer c.Close()
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
